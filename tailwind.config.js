9f220762807698f60b2528d60360bd7f6734cdba
sibbng <sibbngheid@gmail.com>
2021-02-18 22:02:12 +0300

    upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-  future: 'all',
-  experimental: 'all',
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: ["index.html", "src/**/*.vue", "src/**/*.js"],
-    options: {
-      whitelistPatterns: [/data-theme$/],
-    },
-  },
+  // future: 'all',
+  // experimental: 'all',
+  // purge: {
+  //   enabled: process.env.NODE_ENV === 'production',
+  //   content: ["index.html", "src/**/*.vue", "src/**/*.js"],
+  //   options: {
+  //     whitelistPatterns: [/data-theme$/],
+  //   },
+  // },
   variants: {
     margin: ["responsive", "hover", "focus"],
     opacity: ["responsive", "hover", "focus", "group-hover"],
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vueSvgPlugin from "vite-plugin-vue-svg"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueSvgPlugin(), ...WindiCSS()],
+  plugins: [vue(), vueSvgPlugin(), WindiCSS()],
   resolve: {
     alias: {
       "/~/": "/src/",
